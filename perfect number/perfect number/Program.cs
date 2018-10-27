791d4c8ef824357aa7d2175a45a69646799a7a26
MAbuElmagd <43254148+MAbuElmagd@users.noreply.github.com>
2018-10-27 23:01:46 +0200

    Add files via upload

---
+++ perfect number/perfect number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> x = new List<int>();
+            for (int i = 1; i <= 1000; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        x.Add(j);
+                }
+                if(x.Sum()==i)
+                    Console.WriteLine(i);
+                x.Clear();
+            }
+            Console.ReadKey();
+        }
+    }
+}
